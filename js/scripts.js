2c08bfec3a0989e6e032a61dd78ba5a6c66b56ae
thuyhien19 <caothuyhien2003@gmail.com>
2024-08-30 18:59:48 +0700

    Fixed

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,34 @@ const taskList = document.querySelector('.task-list');
 function AddTask() {
     if(inputBox.value === ''){
         alert("Hay Nhap 1 Cong Viec ");
-    }else { 
+    }
+    else { 
         let li = document.createElement('li');
         li.innerHTML = inputBox.value; 
         taskList.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = 'X';
+        li.appendChild(span);
     }
+    inputBox.value = '';
+    saveData()
 }
+
+taskList.addEventListener('click', function(e) { 
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false
+);
+
+function saveData() { 
+    localStorage.setItem("data", taskList.innerHTML);
+};
+function sTask() {
+    taskList.innerHTML = localStorage.getItem("data") || '';
+}
+sTask();
